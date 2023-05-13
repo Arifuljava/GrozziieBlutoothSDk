bb970c1c55a41d8cfa4deaadc15558913320b80a
Arifuljava <arifulislam.java.c@gmail.com>
2023-05-13 16:18:09 +0600

    PPPLM

--- app/src/main/java/com/grozziie/bluetoothsdk/MainActivity.java
+++ app/src/main/java/com/grozziie/bluetoothsdk/MainActivity.java
@@ -2,7 +2,10 @@ package com.grozziie.bluetoothsdk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // startActivity(new Intent(getApplicationContext(), InitilizeSDKActivity.class));
 
     }
 }
